a90d19817fc09462ba6327dead30d8ed4676c330
jutturong <jutturongcharoenrit@gmail.com>
2017-02-14 11:43:24 +0700

    update report 1 14-2-60

--- document/report_pdf/docdb/querydb.php
+++ document/report_pdf/docdb/querydb.php
@@ -264,12 +264,12 @@ function  chstr1($t)
 $type_record=$_REQUEST["type_record"];
 $type_document=$_REQUEST["type_document"];
   
-//$cur_date=date("Y-m-d");    and   `date` = '$cur_date' 
+$cur_date=date("Y-m-d");  
     
 if(  $type_record >= 1  &&  $type_document >= 1 )
 {
        $tb="tb_main1";
-       $str="select  *  from  $tb  where  type_record=$type_record  and   type_document=$type_document      ";
+       $str="select  *  from  $tb  where  type_record=$type_record  and   type_document=$type_document     and   `date` = '$cur_date'      ";
        $query=mysql_query($str) or die("mysql error ");
        
        /*
